c3c49dd956441f3353016aac6b82f4fe0d610ece
remynixu <remynixudev@gmail.com>
2025-02-12 11:51:25 +0800

    auto pushed

--- main.c
+++ main.c
@@ -1,13 +1,1 @@
-#include "./mod/display/icon.h"
-#include "./mod/display/graphic.h"
-#include "./mod/display/color.h"
 
-#define ENTRY_NAME	main
-#define WNDWIDTH	40
-#define WNDHEIGHT	20
-#define WNDSIZE		(WNDWIDTH * WNDHEIGHT)
-
-int ENTRY_NAME(void){
-	puticon(mkicon(INVALCLR, INVALGB));
-	return 0;
-}
